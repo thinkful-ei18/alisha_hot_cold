6d0989023809e38b261d6571e75c9264f1a0cbb4
alishaevn <alieva.inc@gmail.com>
2018-02-26 12:59:01 -0800

    created board component and updated index.js to render it

--- src/components/board.js
+++ src/components/board.js
@@ -1,6 +1,19 @@
-'use strict';
-
 /*********
  * nav component
  * game component
  *********/
+
+import React from 'react';
+// import Nav from './nav';
+// import PlayingField from './playingField';
+
+
+export default function Board(props) {
+  return (
+    <div>
+      {/* <div><Nav /></div>
+      <div><PlayingField /></div> */}
+      <p>Hello World</p>
+    </div>
+  )
+}

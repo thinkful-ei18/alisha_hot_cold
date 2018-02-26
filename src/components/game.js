54b4b43f64b5573598d9a7e525b2dac0489de716
alishaevn <alieva.inc@gmail.com>
2018-02-26 13:04:35 -0800

    created stateless component

--- src/components/game.js
+++ src/components/game.js
@@ -1,8 +1,23 @@
-'use strict';
-
 /*********
  * hot and cold title
  * responses component
  * playingField component
  * priorGuesses component
  *********/
+
+import React from 'react';
+import Responses from './responses';
+import PlayingField from './playingField';
+import PriorGuesses from './priorGuesses';
+
+
+export default function Game() {
+  return (
+    <div>
+      <h1>HOT or COLD</h1>
+      <div><Responses /></div>
+      <div><PlayingField /></div>
+      <div><PriorGuesses /></div>
+    </div>
+  )
+}

4467a50a5a0d2a2f8781c15ce11a9f30afaa29be
alishaevn <alieva.inc@gmail.com>
2018-02-26 13:15:58 -0800

    created stateless component

--- src/components/playingField.js
+++ src/components/playingField.js
@@ -1,7 +1,20 @@
-'use strict';
-
 /*********
  * form to enter guesses 
  * submit button
  * guess #
  *********/
+
+import React from 'react';
+
+
+export default function PlayingField() {
+  return (
+    <div>
+      <form>
+        <input type='text' placeholder='enter your guess'></input>
+        <input type='submit'></input>
+      </form>
+      <p>Guess #0!</p>
+    </div>
+  )
+}

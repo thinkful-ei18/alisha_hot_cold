029115f75d8ba6b209670327a241176e4ae7ea4d
alishaevn <alieva.inc@gmail.com>
2018-02-26 13:18:54 -0800

    created stateless component

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,6 +1,20 @@
-'use strict';
-
 /*********
  * 'what' link
  * 'new game' link
  *********/
+
+
+import React from 'react';
+
+export default function Game() {
+  return (
+    <ul>
+      <li>
+        <a href=''>What?</a>
+      </li>
+      <li>
+        <a href=''>New Game+</a>
+      </li>
+    </ul>
+  )
+}

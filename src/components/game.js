761e75cedbc6bcef7d5ffa97b3fffd871b633e31
alishaevn <alieva.inc@gmail.com>
2018-03-01 08:26:41 -0800

    converted into class and added state

--- src/components/game.js
+++ src/components/game.js
@@ -5,20 +5,34 @@
  * priorGuesses component
  *********/
 
-import React from 'react';
+import React, {Component} from 'react';
 import Responses from './responses';
 import PlayingField from './playingField';
 import PriorGuesses from './priorGuesses';
 import './game.css';
 
 
-export default function Game() {
-  return (
-    <div className="game">
-      <h1>HOT or COLD</h1>
-      <div><Responses /></div>
-      <div><PlayingField /></div>
-      <div><PriorGuesses /></div>
-    </div>
-  )
+export default class Game extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state= {
+      guess: '',
+      lastGuess: '',
+      allGuesses: [],
+      responses: ['cold', 'warm', 'very warm', 'hot']
+    }
+  }
+
+  render() {
+    return (
+      <div className="game">
+        <h1>HOT or COLD</h1>
+        <div><Responses /></div>
+        <div><PlayingField /></div>
+        <div><PriorGuesses /></div>
+      </div>
+    )
+  }
+  
 }

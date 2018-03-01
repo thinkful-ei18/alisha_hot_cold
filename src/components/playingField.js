3f715927842c3ed82c63445792a3842002d25ef0
alishaevn <alieva.inc@gmail.com>
2018-03-01 10:38:48 -0800

    removed comments

--- src/components/playingField.js
+++ src/components/playingField.js
@@ -20,23 +20,3 @@ export default function PlayingField(props) {
   )
 }
 
-/*
-onSubmit -
-  form: target shows as null in the console
-  input: refreshes the page with no console log
-  button: refreshes the page with no console log
-
-onClick - 
-  form: target shows as null on the click and the submit
-  input: console logs when I click in the text box, but refreshes upon submit
-  button: target shows as null in the console
-
-onChange -
-input: target is null
-
-
-      <form className='form'>
-        <input className="guess" type="text" placeholder="enter your guess" onChange={(e) => props.currentGuess(e)}/><br></br>
-        <button className="submit" type="submit" onClick={e => e.preventDefault()}>Guess</button>
-      </form>
-*/
